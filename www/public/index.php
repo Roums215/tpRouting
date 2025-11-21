1921ce2b11d0225ace07f6f8c28cb11b4e4884f3
Ionita <ionita@MacBook-Pro-de-Ionita.local>
2025-11-21 08:10:01 +0100

    index.php

--- www/public/index.php
+++ www/public/index.php
@@ -1,23 +1,43 @@
 <?php
 
-/*
- *
- * TP : Routing
- *
- * Faire en sorte que toutes les requêtes HTTP pointent sur le fichier index.php se trouvant dans public
- * Se baser ensuite sur le fichier routes.yml pour appeler la bonne classe dans le dossier controller et
- * la bonne methode (ce que l'on appel une action dans un controller)
- *
- * Exemple :
- * http://localhost:8080/contact
- * Doit créer une instance de Base et appeler la méthode (action) : contact
- *
- * Pensez à effectuer tous les nettoyages et toutes les vérifications pour
- * afficher des erreurs (des simples die suffiront dans un premier temps)
- *
- * Rendu : Mail y.skrzypczyk@gmail.com
- * Objet du mail : 3IW1 - TP routing - Nom Prénom
- * Contenu du mail : fichier index.php et les autres fichiers créés s'il y en a
- *
- * Bon courage
- */
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+include '../Controllers/Auth.php';
+include '../Controllers/Base.php';
+
+
+$url = strtok($_SERVER['REQUEST_URI'] ?? '/', '?');
+if ($url === '' || $url === false) {
+    $url = '/';
+}
+
+
+if (!preg_match('#^/[a-zA-Z0-9/_-]$#', $url)) {
+    die('url pas bonne');
+}
+
+$routesFile = __DIR__ . '/../routes.yml';
+
+
+
+$routes = yaml_parse_file($routesFile);
+
+
+
+if (!isset($routes[$url])) {
+    die('Fais demi tour');
+}
+
+
+$controllerClass = $routes[$url]['controller'] ?? null;
+$action          = $routes[$url]['action'] ?? null;
+
+
+
+
+$controller = new $controllerClass();
+
+
+
+$controller->$action();
